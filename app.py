6bb3bee6c0b57904a7cc2bb15efa021d112c4aed
Amir <amir.gabbay@gmail.com>
2024-09-13 17:46:49 +0200

    added email connection process

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ import sys
 import tkinter as tk
 from tkinter import filedialog
 import json
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.oauth2.credentials import Credentials
+import pickle
+from google.auth.transport.requests import Request
 
 # Function to select a folder and save its path to the config.json file
 def choose_folder():
@@ -23,6 +27,28 @@ def choose_folder():
         print("No folder selected.")
         sys.exit()
 
+# Function to authenticate and get Gmail credentials
+def get_gmail_credentials():
+    SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
+    credentials_file = 'client_secret.json'  # Path to your OAuth 2.0 credentials file
+    token_file = 'token.pickle'  # File where access tokens will be stored
+
+    creds = None
+    if os.path.exists(token_file):
+        with open(token_file, 'rb') as token:
+            creds = pickle.load(token)
+    
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(credentials_file, SCOPES)
+            creds = flow.run_local_server(port=0)
+        with open(token_file, 'wb') as token:
+            pickle.dump(creds, token)
+
+    return creds
+
 # Determine the base path
 if getattr(sys, 'frozen', False):
     # Running as a PyInstaller bundle
@@ -30,7 +56,6 @@ if getattr(sys, 'frozen', False):
 else:
     # Running as a normal Python script
     base_path = os.path.dirname(os.path.abspath(__file__))
-
 
 # Path to the JSON file where folder path will be saved
 config_file_path = os.path.join(os.path.expanduser("~"), 'config.json')
@@ -46,8 +71,9 @@ else:
     # If config.json doesn't exist, prompt the user to select a folder
     invoices_folder = choose_folder()
 
-
-gmail = Gmail() # will open a browser window to ask you to log in and authenticate for the first time
+# Get Gmail credentials
+creds = get_gmail_credentials()
+gmail = Gmail()  # Pass the credentials to the Gmail class
 
 query_params = { # select the list of email you want to get from the Gmail inbox
     "newer_than": (7, "day"),
@@ -57,13 +83,13 @@ query_params = { # select the list of email you want to get from the Gmail inbox
 mails = gmail.get_messages(query=construct_query(query_params)) # run the query and get list of emails
 
 for message in mails:
-    safe_filename = re.sub(r'[<>:"/\\|?*]', '_', message.subject + message.date) # change the file name to a safe name so you cna save it on your pc
+    safe_filename = re.sub(r'[<>:"/\\|?*]', '_', message.subject + message.date) # change the file name to a safe name so you can save it on your pc
     file_path = invoices_folder + '\\' + safe_filename + '.pdf' # set the new pdf name
     if os.path.exists(file_path): # checks if the pdf name already exists in the directory, if not continue
         print('All invoices are saved!')
         break
     else: 
-        if any (word in message.subject for word in ['חשבונית', 'invoice','receipt','קבלה','bill']): # if the email subject contains these words continue 
+        if any(word in message.subject for word in ['חשבונית', 'invoice', 'receipt', 'קבלה', 'bill']): # if the email subject contains these words continue 
             if message.attachments: # if the mail contains pdf/image/file save the attachments
                 for attm in message.attachments:
                     attm.save(filepath=file_path) # save the file
